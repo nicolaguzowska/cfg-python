59443ed02db56780a9bd5aaa79d084e2b0623010
Nicola Guzowska <nicola.guzowska@gmail.com>
2018-11-29 18:48:46 +0000

    Add new route and styling

--- flask-learn.py
+++ flask-learn.py
@@ -5,14 +5,19 @@ app = Flask("MyApp")
 def hello():
     return "Hello World"
 
-@app.route("/<name>")
-def hello_someone(name):
-        return render_template("hello.html", name=name.title())
+
+@app.route("/goodbye")
+def goodbye():
+        return render_template("bye.html")
         
 @app.route("/signup", methods=["POST"])
 def sign_up():
     form_data = request.form
     print form_data["email"]
     return "All OK"
+
+@app.route("/<name>")
+def hello_someone(name):
+        return render_template("hello.html", name=name.title())
 
 app.run(debug=True)
